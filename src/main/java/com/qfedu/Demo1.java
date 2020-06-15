69d7e552c140129c903c42d67d259bf1848502ac
gxb <550913664@qq.com>
2020-06-15 08:36:02 +0800

    添加Demo1的属性id

---
+++ src/main/java/com/qfedu/Demo1.java
@@ -0,0 +1,14 @@
+package com.qfedu;
+
+/**
+ * @PackgeName: com.qfedu
+ * @ClassName: Demo1
+ * @Author: gxb
+ * Date: 2020/6/15 8:35:11
+ * project name: demo1
+ * @Version:
+ * @Description:
+ */
+public class Demo1 {
+    private int id;
+}
